12735de1949403c6ae7ba03cb0febc2b166d69bf
antish24 <aantenehh@gmail.com>
2024-05-22 05:21:56 -0700

    done with physician pharmacy casher triage 70 percent 2

--- src/components/description/PrescriptionInfo.js
+++ src/components/description/PrescriptionInfo.js
@@ -13,7 +13,7 @@ const PrescriptionInfo = ({data}) => {
   return (
     <div>
         <div style={{display:'flex',justifyContent:'flex-end'}}><span style={{fontWeight:'bold'}}>Date : </span><span style={{marginLeft:'5px'}}>{FormatDateTime(data.createdAt)}</span></div>
-        <div style={{display:'flex',justifyContent:'flex-end',marginBottom:'10px'}}><span style={{fontWeight:'bold'}}>Status : </span><Tag style={{marginLeft:'5px'}} color={data.status==='Completed'?'green':data.status==='Pending'?'yellow':'red'}>'status'</Tag></div>
+        <div style={{display:'flex',justifyContent:'flex-end',marginBottom:'10px'}}><span style={{fontWeight:'bold'}}>Status : </span><Tag style={{marginLeft:'5px'}} color={data.status==='Completed'?'green':data.status==='Pending'?'yellow':'red'}>status</Tag></div>
         <Collapse items={items} bordered={false} defaultActiveKey={['1']} />
         <div style={{display:'flex',justifyContent:'flex-end'}}><span style={{fontWeight:'bold'}}>Prescribed by </span> : {data.physicianId}</div>
         <div style={{display:'flex',justifyContent:'flex-end'}}>Updated At : {data.updateAt}</div>
